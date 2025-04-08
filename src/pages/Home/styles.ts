9d0fb0b37e5f36b417486bbb9f014c89f7a2fee7
pedro-cruzz <pedrocruzvb123@gmail.com>
2025-04-08 15:16:23 -0300

    feat: adicionar novos estilos e textos ao componente Home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -27,3 +27,39 @@ export const TextForm = styled.p`
     max-width: 882px;
     font-family: ${theme.fonts.switzer};
 `;  
+
+export const TitleBanner6 = styled.h1`
+    display: flex;
+    max-width: 530px;
+    width: 100%;
+    position: absolute;
+    margin-left: 800px;
+    color: ${theme.colors.WHITE};
+    font-size: 40px;
+    font-weight: 700;
+    font-family: ${theme.fonts.boska};
+    margin-botom: 2rem;
+    margin-top: 10rem;
+    algin-items: center;
+    text-justify: left;
+`; 
+
+export const TextBanner6 = styled.p`
+    display: flex;
+    max-width: 500px;
+    width: 100%;
+    position: absolute;
+    margin-left: 800px;
+    margin-top: 15rem;
+    font-size: 20px;
+    font-weight: 400;
+    font-family: ${theme.fonts.switzer};
+    justify-content: center;
+    align-items: center;
+    text-align: left;
+    flex-wrap: wrap;
+    flex-direction: column;
+    gap: 3px;
+    color: ${theme.colors.WHITE};
+`;
+
